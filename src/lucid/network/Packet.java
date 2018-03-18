4c67312bc3cdf9ab372c07e7c4eaf2749a3ea2bd
Julian Thijssen <Nimthora@gmail.com>
2018-03-18 14:43:31 +0100

    Add packet packing/unpacking test and fix integer unpacking bug

--- src/lucid/network/Packet.java
+++ src/lucid/network/Packet.java
@@ -154,7 +154,11 @@ public class Packet {
 		b[1] = data[pos++];
 		b[2] = data[pos++];
 		b[3] = data[pos++];
-		return b[0] << 24 | b[1] << 16 | b[2] << 8 | b[3] << 0;
+
+		return  b[0] << 24 & 0xFF000000 |
+				b[1] << 16 & 0x00FF0000 |
+				b[2] << 8  & 0x0000FF00 |
+				b[3] << 0  & 0x000000FF;
 	}
 	
 	public float getFloat() {
---
+++ tests/tests/PacketTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import lucid.network.Packet;
+
+public class PacketTest {
+
+	@Test
+	public void test() {
+		// Integer test
+		for (int a = 0; a < 100000; a += 100) {
+			Packet packet = new Packet(0);
+			
+			packet.addInt(a);
+			packet.reset();
+			int b = packet.getInt();
+			
+			if (a != b) {
+				fail("Packet integer test fail: " + a + " != " + b);
+			}
+		}
+		
+		// Float test
+		for (float a = 0; a < 100000; a += 100) {
+			Packet packet = new Packet(0);
+			
+			packet.addFloat(a);
+			packet.reset();
+			float b = packet.getFloat();
+			
+			if (a != b) {
+				fail("Packet float test fail: " + a + " != " + b);
+			}
+		}
+	}
+
+}
