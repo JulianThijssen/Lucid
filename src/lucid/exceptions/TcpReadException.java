3bc112424389210a6ce8810688da5181a74d54d1
Julian Thijssen <Nimthora@gmail.com>
2018-06-04 22:35:38 +0200

    Remove TCP exceptions

--- src/lucid/exceptions/TcpReadException.java
+++
@@ -1,7 +0,0 @@
-package lucid.exceptions;
-
-public class TcpReadException extends Exception {
-	public TcpReadException(String message) {
-		super(message);
-	}
-}
--- src/lucid/exceptions/TcpWriteException.java
+++
@@ -1,7 +0,0 @@
-package lucid.exceptions;
-
-public class TcpWriteException extends Exception {
-	public TcpWriteException(String message) {
-		super(message);
-	}
-}
