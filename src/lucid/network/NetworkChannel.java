3fd28492a5e0bd1ec0ae32e8356ae2ff360546db
Julian Thijssen <Nimthora@gmail.com>
2018-06-04 23:13:21 +0200

    Add NetworkChannel interface

---
+++ src/lucid/network/NetworkChannel.java
@@ -0,0 +1,22 @@
+package lucid.network;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.nio.channels.ClosedChannelException;
+
+import lucid.exceptions.ChannelReadException;
+import lucid.exceptions.ChannelWriteException;
+
+public interface NetworkChannel {
+	public void setBlocking(boolean blocking) throws IOException;
+	public boolean isConnected();
+	public SocketAddress getRemoteAddress() throws ClosedChannelException, IOException;
+	
+	public void send(Packet packet);
+	public Packet receive();
+	
+	public void write() throws ChannelWriteException;
+	public void read() throws ChannelReadException;
+	
+	public void close() throws IOException;
+}
