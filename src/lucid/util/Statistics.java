d719fb275f6dd6e31881f55bc9f02435bb0a0a61
Julian Thijssen <Nimthora@gmail.com>
2018-06-05 12:16:44 +0200

    Add Statistics class to keep track of some running statistics

---
+++ src/lucid/util/Statistics.java
@@ -0,0 +1,8 @@
+package lucid.util;
+
+import java.util.function.IntSupplier;
+
+public class Statistics {
+	public static IntSupplier inPacketBufferSize;
+	public static IntSupplier outPacketBufferSize;
+}
